ef8e6ddaa7a25c6d602c153b7c5e2ffa404e2fb8
kArAn-rAwAt24 <ks573696@gamil.com>
2023-07-06 12:44:43 +0530

    created shop preview and seprate category page using nested routes

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,15 +1,15 @@
-import { useContext } from 'react';
-import { ProductsContext } from '../../contexts/product.context';
-import Productcard from '../../components/product-card/product-card.component';
-import './shop.styles.scss'
+import { Route, Routes } from 'react-router-dom';
+import './shop.styles.scss';
+
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
+
 const Shop = () => {
-  const { products } = useContext(ProductsContext);
   return (
-    <div className='products-container'>
-      {products.map((product) => {
-        return <Productcard key={product.id} product={product} />;
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=':category' element={<Category />} />
+    </Routes>
   );
 };
 
